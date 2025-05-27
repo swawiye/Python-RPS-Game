55ea30785ec89c2c2fea6aa7a415ab83117fc59e
swawiye <sovereign.wawiye@gmail.com>
2025-05-27 17:40:04 +0300

    initialization

---
+++ game.py
@@ -0,0 +1,8 @@
+print("Welcome to the RPS Game. Choose either rock, paper, or scissors")
+y = input("Choose your weapon:")
+# Variables
+r = "rock"
+p = "paper"
+s = "scissors"
+if y == r:
+    print()
