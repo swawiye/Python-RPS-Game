bd238b9a3738c714b8be0303c96d878ba88a3d12
swawiye <sovereign.wawiye@gmail.com>
2025-05-27 19:41:25 +0300

    logic

--- game.py
+++ game.py
@@ -1,8 +1,32 @@
 print("Welcome to the RPS Game. Choose either rock, paper, or scissors")
-y = input("Choose your weapon:")
+
+choice = input("Choose your weapon:")
+
 # Variables
 r = "rock"
 p = "paper"
 s = "scissors"
-if y == r:
-    print()
+
+# Rock
+if choice == r:
+    print("You chose {choice}. It's a tie.")
+elif choice == s:
+    print("You chose {choice}. You lose.")
+else:
+    print("You chose {choice}. You win!")
+
+# Paper
+if choice == p:
+    print("You chose {choice}. It's a tie.")
+elif choice == r:
+    print("You chose {choice}. You lose.")
+else:
+    print("You chose {choice}. You win!")
+    
+# Scissors
+if choice == s:
+    print("You chose {choice}. It's a tie.")
+elif choice == r:
+    print("You chose {choice}. You lose.")
+else:
+    print("You chose {choice}. You win!")
