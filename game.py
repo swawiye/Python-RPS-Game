37168de8268670d08143ac5287fc938a323bb048
swawiye <sovereign.wawiye@gmail.com>
2025-05-28 07:41:33 +0300

    output and data validation

--- game.py
+++ game.py
@@ -1,43 +1,53 @@
-print("Welcome to the RPS Game. Choose either rock, paper, or scissors")
+print("Welcome to the RPS Game.")
 print("Rules")
 print("1. Rock defeats Scissors")
 print("2. Paper defeats Rock")
 print("3. Scissors defeat Paper")
 
-choice = input("Choose your weapon:")
-
-# Variables
-r = "rock"
-p = "paper"
-s = "scissors"
-
-# Rock
-if choice == r:
-    print("You chose {choice}. It's a tie.")
-elif choice == s:
-    print("You chose {choice}. You lose.")
-else:
-    print("You chose {choice}. You win!")
-
-# Paper
-if choice == p:
-    print("You chose {choice}. It's a tie.")
-elif choice == r:
-    print("You chose {choice}. You lose.")
-else:
-    print("You chose {choice}. You win!")
-    
-# Scissors
-if choice == s:
-    print("You chose {choice}. It's a tie.")
-elif choice == r:
-    print("You chose {choice}. You lose.")
-else:
-    print("You chose {choice}. You win!")
+#choice = input()
+#
+## Variables
+#r = "rock"
+#p = "paper"
+#s = "scissors"
+#
+## Rock
+#if choice == r:
+#    print("You chose {choice}. It's a tie.")
+#elif choice == s:
+#    print("You chose {choice}. You lose.")
+#else:
+#    print("You chose {choice}. You win!")
+#
+## Paper
+#if choice == p:
+#    print("You chose {choice}. It's a tie.")
+#elif choice == r:
+#    print("You chose {choice}. You lose.")
+#else:
+#    print("You chose {choice}. You win!")
+#    
+## Scissors
+#if choice == s:
+#    print("You chose {choice}. It's a tie.")
+#elif choice == r:
+#    print("You chose {choice}. You lose.")
+#else:
+#    print("You chose {choice}. You win!")
 
 import random
 
 options = ("rock", "paper", "scissors")
+
 player = None 
 computer = random.choice(options) # pick a random choice from options
+
+while player not in options: # encures that the player only picks one of the presented options
+    player = input("Choose your weapon(rock, paper, scissors): ") # player's choice
+
+print(f"Player: {player}")
+print(f"Computer: {computer}")
+
+if player == computer:
+    print("It's a tie.")
 
