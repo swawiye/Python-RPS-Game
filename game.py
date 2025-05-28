f2638da399a43220a35d1a60ac0b5263ec406074
swawiye <sovereign.wawiye@gmail.com>
2025-05-28 07:34:47 +0300

    rules and random method

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
 print("Welcome to the RPS Game. Choose either rock, paper, or scissors")
+print("Rules")
+print("1. Rock defeats Scissors")
+print("2. Paper defeats Rock")
+print("3. Scissors defeat Paper")
 
 choice = input("Choose your weapon:")
 
@@ -30,3 +34,10 @@ elif choice == r:
     print("You chose {choice}. You lose.")
 else:
     print("You chose {choice}. You win!")
+
+import random
+
+options = ("rock", "paper", "scissors")
+player = None 
+computer = random.choice(options) # pick a random choice from options
+
